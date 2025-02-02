f03ed9fade889eeddf98e5c941dfff93427869c8
henrybza <henry3544@hotmail.com>
2025-02-01 21:02:27 -0600

    declaracion de variables

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// declaracion de variables a utilizar
+
+let amigo  = []; // definimos la variable que contendra los nombres.
