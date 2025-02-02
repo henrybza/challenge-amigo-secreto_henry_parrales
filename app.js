f1c7e7d8bfb1f1d42feac05ff457449ea798d215
henrybza <henry3544@hotmail.com>
2025-02-01 21:16:18 -0600

    creacion de funcion registrar nombres

--- app.js
+++ app.js
@@ -2,3 +2,17 @@
 // declaracion de variables a utilizar
 
 let amigo  = []; // definimos la variable que contendra los nombres.
+
+// funcion que me permite añadir los nombres a la variable amigo
+function registrarNombre() {
+  let  nombre=document.getElementById('amigo').value;
+
+if (nombre!="") {
+amigo.push(nombre);
+document.getElementById('amigo').value="";
+
+}else {
+    alert("por favor, inserte un nombre");
+}
+
+}
